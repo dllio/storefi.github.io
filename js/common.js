12a1a207e3f111c8260f4695412b9945b5f6bebb
dllio <58674977+dllio@users.noreply.github.com>
2022-05-15 09:43:34 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,36 @@
+
+ $(function () {
+    
+    //   fnTopMenu();
+    //   window.onscroll = function () {
+    //     fnTopMenu();
+    // }
+     
+
+   
+
+ 
+ })
+
+ function fnIsToggle() {
+    $("#sy-menu").addClass("active");
+  }
+  function fnClose() {
+    $("#sy-menu").removeClass("active");
+  }
+
+//  function fnTopMenu(){
+//     var t = document.documentElement.scrollTop || document.body.scrollTop; //变量t就是滚动条滚动时，到顶部的距离
+
+//     var left = document.getElementById("g-header"); 
+//     if (t >= 80) {     //当拖动到距离顶部100px处时，左边导航固定，不随滚动条滚动
+
+//       left.style.background = "#fff"; 
+
+//     } else {
+
+//       left.style.background = "#fff"; 
+
+//     }
+  
+//   }
---
+++ js/font.js
@@ -0,0 +1,25 @@
+fontSize();
+$(window).resize(function () {
+    fontSize();
+});
+
+function fontSize() {
+    var size;
+    var winW = $(window).width();
+    if (winW <= 1600 && winW > 800) {
+        size = Math.round(winW / 16);
+    } else if (winW <= 800) {
+        size = Math.round(winW / 7.5);
+        if (size > 65) {
+            size = 65;
+        }
+    } else {
+        size = 100;
+    }
+    $('html').css({
+        'font-size': size + 'px'
+    });
+}
+
+
+
